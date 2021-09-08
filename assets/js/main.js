e9ad5c55bee4f75ad130ea81d9fa2a70e3376798
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-08 19:58:47 +0530

    All Plots have been completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,7 @@ fetch('data/json/total_run_by_team.json')
       });
   });
 
+
 // Total Runs by player scored for Royal Challanger Banglore is here
 fetch('data/json/player_runs_by_team.json')
   .then(response => response.json())
@@ -50,6 +51,7 @@ fetch('data/json/player_runs_by_team.json')
           }]
       });
   });
+
 
 // Foreign Umpire Analysis is here
 fetch('data/json/umpire_analysis.json')
@@ -77,76 +79,71 @@ fetch('data/json/umpire_analysis.json')
       });
   });
 
-// $(document).ready( ()=> {
-//     Highcharts.chart('container', {
-//         chart: {
-//           type: 'column'
-//         },
-//         // title: {
-//         //   text: 'World\'s largest cities per 2017'
-//         // },
-//         // subtitle: {
-//         //   text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
-//         // },
-//         // xAxis: {
-//         //   type: 'category',
-//         //   labels: {
-//         //     rotation: -45,
-//         //     style: {
-//         //       fontSize: '13px',
-//         //       fontFamily: 'Verdana, sans-serif'
-//         //     }
-//         //   }
-//         // },
-//         // yAxis: {
-//         //   min: 0,
-//         //   title: {
-//         //     text: 'Population (millions)'
-//         //   }
-//         // },
-//         // legend: {
-//         //   enabled: false
-//         // },
-//         // tooltip: {
-//         //   pointFormat: 'Population in 2017: <b>{point.y:.1f} millions</b>'
-//         // },
-//         series: [{
-//           name: 'Population',
-//           data: [
-//             ['Shanghai', 24.2],
-//             ['Beijing', 20.8],
-//             ['Karachi', 14.9],
-//             ['Shenzhen', 13.7],
-//             ['Guangzhou', 13.1],
-//             ['Istanbul', 12.7],
-//             ['Mumbai', 12.4],
-//             ['Moscow', 12.2],
-//             ['São Paulo', 12.0],
-//             ['Delhi', 11.7],
-//             ['Kinshasa', 11.5],
-//             ['Tianjin', 11.2],
-//             ['Lahore', 11.1],
-//             ['Jakarta', 10.6],
-//             ['Dongguan', 10.6],
-//             ['Lagos', 10.6],
-//             ['Bengaluru', 10.3],
-//             ['Seoul', 9.8],
-//             ['Foshan', 9.3],
-//             ['Tokyo', 9.3]
-//           ],
-//         //   dataLabels: {
-//         //     enabled: true,
-//         //     rotation: -90,
-//         //     color: '#FFFFFF',
-//         //     align: 'right',
-//         //     format: '{point.y:.1f}', // one decimal
-//         //     y: 10, // 10 pixels down from the top
-//         //     style: {
-//         //       fontSize: '13px',
-//         //       fontFamily: 'Verdana, sans-serif'
-//         //     }
-//         //   }
-//         }]
-//       });
-// });
+
+// Matches Played By Teams in Every Season is here
+fetch('data/json/matches_played_by_season.json')
+  .then(response => response.json())
+  .then(data => {
+
+    // Create a temprary array to store all year and teams
+      let boolean = false;
+      let all_years = [];
+      let all_teams = [];
+
+      for (item of Object.entries(data)){
+            if (!boolean){
+                for (i of Object.entries(item[1])){
+                    all_years.push(i[0])
+                }
+                boolean = true
+            }
+            all_teams.push(item[0])
+        }
+
+    //   To Store all series for plotting
+      let series = [];
+
+      for (year of all_years){
+          let values = []
+          for (team of all_teams){
+              values.push(data[team][year])
+          }
+          series.push({
+              name: `${year}`,
+              data: values
+          })
+      }
+
+
+      Highcharts.chart('matches-played-by-team', {
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: 'Matches Played By Teams in Seasons'
+          },
+          xAxis: {
+              type: 'category',
+              categories: all_teams
+          },
+          yAxis: {
+            title: {
+                  text: 'Count'
+            },
+            stackLabels: {
+                enabled: true
+            }
+          },
+          plotOptions: {
+            column: {
+              stacking: 'normal',
+              dataLabels: {
+                enabled: true
+              }
+            }
+          },
+          series: series
+      });
+  });
+
   
